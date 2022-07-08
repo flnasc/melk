6ff66dabcfea6de6f5847272794909f1754920a4
Zeb <zebfullerbecker@gmail.com>
2022-07-08 16:27:32 -0400

    Adds error handling for empty FULL_TEXT fields.

--- cleaner.py
+++ cleaner.py
@@ -9,21 +9,27 @@ def main(filename):
 
     # drop rows with no text
     df = df[df.FULL_TEXT.notnull()]
+    df.dropna(subset=["FULL_TEXT"])
     # df = df[clean(df.FULL_TEXT)]
 
     # should switch to vectorized operation
     for i in range(len(df)):
-        text = df.at[i, "FULL_TEXT"]
-        # print("TEXT IS:      \n", text)
-        text = clean(text)
 
-        # How are we implementing the options system? 
+        try:
+            text = df.at[i, "FULL_TEXT"]
+            # print("TEXT IS:      \n", text)
+            text = clean(text)
+            df.at[i, "FULL_TEXT"] = text
+        except KeyError:
+            pass
+
+        # How are we implementing the options system?
         """if lower_flag:
                 text = text.lower()
             if remove_hashtags_flag:
-                text = remove_hashtags(text)"""
-
-        df.at[i, "FULL_TEXT"] = text
+                text = remove_hashtags(text)
+            if remove_twitter_handles_flag:
+                text = remove_twitter_handles(text)"""
 
     #  No such file or directory: 'clean_./outputs/metaverse_2022-05-01_2022-05-02.csv'
     # filename_out = "clean_" + filename
@@ -47,15 +53,29 @@ def clean(text):
     # print(text)
     return text
 
+
 def remove_hashtags(text):
     # this removes just the # character itself:
     text = re.sub(r"#", "", text)
 
-    # to remove the entire #phrase, use this expression: 
+    # to remove the entire #phrase, use this expression:
     # text = re.sub(r"#\S+", "", text)
     return text
 
+
 def remove_twitter_handles(text):
-    #removes twitter handles
+    # removes twitter handles
     text = re.sub(r"@\S+", "", text)
     return text
+
+
+def test():
+    # filename = "./outputs/whiskey_2022-06-20_2022-06-23.csv"
+    filename = "./outputs/clean_example.csv"
+    df = pd.read_csv(filename)
+    print("Before cleaning, ", len(df), " rows.")
+    main(filename)
+    df = pd.read_csv(filename)
+    print("After cleaning, ", len(df), " rows.")
+
+    return
