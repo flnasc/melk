ed1c54f5d739cb39e9201e7573d4b82c5ca74ffa
Zeb <zebfullerbecker@gmail.com>
2022-07-20 12:42:25 -0400

    Adds docstrings.

--- gatherer/engine/reddit_gatherer.py
+++ gatherer/engine/reddit_gatherer.py
@@ -14,7 +14,25 @@ USER_LIMIT = apiconfig.reddit_user_limit
 
 
 def search_reddit(keyword, start_date, end_date, fields):
-    # takes dates as date objects. returns dataframe with collected posts in Melk format (see data).
+    """Searches Reddit archive for posts and comments related to the keyword and within date range. 
+
+    Relies on the Pushshift project for providing access to historical Reddit posts and comments, 
+    and uses the PSAW python wrapper to interact with the Pushshift API. Note that this function will only 
+    return USER_LIMIT posts + comments. While there is not a cap on how many requests we can make to the 
+    Pushshift API, limiting the number of posts + comments that each user can download may be helpful to 
+    limit runtimes. Searching for very common terms over wide date ranges can lead to very long (multiple hours)
+    runtimes if no limit is used. 
+
+    Args: 
+        keyword: string to be searched for. Currently, only one word strings are explicitly supported. 
+        start_date: Datetime object representing first day of search period. 
+        end_date: Datetime object representing last day of search period.
+        fields: list of column headers for eventual csv database. 
+    
+    Returns:
+        df: a Pandas Dataframe (df) containing collected information about each relevant post/comment found.
+            Structured as defined in the file melk_format.py.      
+    """
 
     api = PushshiftAPI()
     logging.basicConfig(filename="melk.log", encoding="utf-8", level=logging.DEBUG)
@@ -43,13 +61,12 @@ def search_reddit(keyword, start_date, end_date, fields):
             logging.info("Downloading Reddit post #%s ....", posts_collected)
 
         try:
-            # do not collect posts with empty bodies (includes cross posts and image/link only posts and deleted/removed posts).
+            # do not collect posts with empty bodies (includes cross posts, image/link only posts, and deleted/removed posts).
             if (
                 post.selftext
                 and post.selftext != "[deleted]"
                 and post.selftext != "[removed]"
             ):
-
                 collect_post(post, data, next_id)
                 posts_collected += 1
                 next_id += 1
@@ -95,6 +112,12 @@ def search_reddit(keyword, start_date, end_date, fields):
 
 
 def collect_post(post, data, next_id):
+    """Converts information from one Reddit post into melk format, appends it as a dict to data.
+
+    Args: 
+        post: post object from generator for this post.
+        data: list of collected posts/comments as dicts of MelkRow objects.  
+    """
     this_post = MelkRow(
         id=next_id,
         source=SOURCE_NAME,
@@ -110,13 +133,19 @@ def collect_post(post, data, next_id):
 
 
 def collect_comment(comment, data, next_id):
+    """Converts information from one Reddit comment into melk format, appends it as a dict to data.
+
+    Args: 
+        comment: comment object from generator for this post. 
+        data: list of collected posts/comments as dicts of MelkRow objects. 
+    """
     this_comment = MelkRow(
         id=next_id,
         source=SOURCE_NAME,
         full_text=comment.body,
         type=COMMENT_TYPE,
         section=comment.subreddit,
-        source_url=comment.id,
+        source_url=comment.id,  # TODO determine how to convert to accessible URL.
         date=(dt.datetime.utcfromtimestamp(comment.created_utc)),
     )
     data.append(vars(this_comment))
